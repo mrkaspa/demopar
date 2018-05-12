e81407326b45d8f3241c56fb3e1bd9de20dd8f98
Michel Perez <michel.ingesoft@gmail.com>
2018-05-11 20:02:28 -0500

    wip

--- test/demopar/core_test.clj
+++ test/demopar/core_test.clj
@@ -1,7 +1,15 @@
 (ns demopar.core-test
   (:require [clojure.test :refer [deftest testing is]]
-            [demopar.pmap :as pmap]))
+            [demopar.pmap :as pmap]
+            [demopar.fail :as fail]
+            [failjure.core :as f]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (> (pmap/dox) 0))))
+
+(deftest a-test
+  (testing "Failjure works"
+    (is (= (fail/validate-name {:id 100}) 100)))
+  (testing "Failjure fails"
+    (is (= (fail/validate-name {}) (f/fail "FAIL")))))
