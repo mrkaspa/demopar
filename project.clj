e66dc79e8f820cfeb8d0e33e6380a0ef7a3c263f
michel perez <michel.ingesoft@gmail.com>
2018-12-07 23:08:05 -0500

    wip

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [com.novemberain/langohr "5.0.0"]
                  [com.fzakaria/slf4j-timbre "0.3.8"]]
   :dev-dependencies [[lein-cljfmt "0.5.7"]]
+  :jvm-opts ["--add-modules" "java.xml.bind"]
   :main ^:skip-aot demopar.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
